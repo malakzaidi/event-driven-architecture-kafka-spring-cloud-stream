ed91253ba21ba3794ebd031e5d5399c520fe9176
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-05 16:12:43 +0100

    create a data analytics real tim stream processing with Kafka streams

--- src/main/java/net/enset/kafkaspringcloudstream/handlers/PageEventhandler.java
+++ src/main/java/net/enset/kafkaspringcloudstream/handlers/PageEventhandler.java
@@ -2,12 +2,20 @@ package net.enset.kafkaspringcloudstream.handlers;
 
 
 import net.enset.kafkaspringcloudstream.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 @Component
@@ -30,5 +38,12 @@ public class PageEventhandler {
                      10+new Random().nextInt(10000)
             );
         };
+    }
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction(){
+        return (input)->
+                        input.filter((k,v)->v.duration()>100)
+                        .map((k,v)->new KeyValue<>(v.name(), v.duration()));
+
     }
 }
