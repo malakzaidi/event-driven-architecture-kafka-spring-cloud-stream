52aceaf49ea53332ce8cf16505001463f4ed04af
malakzaidi <malak.zaidi-etu@etu.univh2c.ma>
2025-10-05 14:32:04 +0100

    create the sevice Supplier Kafka

--- src/main/java/net/enset/kafkaspringcloudstream/handlers/PageEventhandler.java
+++ src/main/java/net/enset/kafkaspringcloudstream/handlers/PageEventhandler.java
@@ -5,7 +5,10 @@ import net.enset.kafkaspringcloudstream.events.PageEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Component
 public class PageEventhandler {
@@ -15,6 +18,17 @@ public class PageEventhandler {
             System.out.println("*******************");
             System.out.println(input.toString());
             System.out.println("*******************");
+        };
+    }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()-> {
+            return new PageEvent(
+                     Math.random()>0.5?"p1":"p2",
+                     Math.random()>0.5?"U1":"U2",
+                     new Date(),
+                     10+new Random().nextInt(10000)
+            );
         };
     }
 }
